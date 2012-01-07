f737de2fe5323aadec8c780dacc11971d3d4634a
Tom Wambold <tom5760@gmail.com>
2012-01-07 17:43:30 -0500

    Initial prototype of web-based GUI.

---
+++ static/js/tnote.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    //console.log("Hello world!");
+    //$.getJSON('/note/start', function (data) {
+    //    console.log(data);
+    //});
+    $('#accordion').accordion({header: 'h3', fillSpace: true});
+});
