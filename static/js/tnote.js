8882a9ca3cbfb046a8aef72f8a86e850313d7b84
Tom Wambold <tom5760@gmail.com>
2012-01-10 18:05:52 -0500

    Clean up link fixing function.
    Better supports internal/external links now.

--- static/js/tnote.js
+++ static/js/tnote.js
@@ -1,34 +1,50 @@
+function getItem(path) {
+    console.log('Requesting:', path);
+    $.getJSON(path, function(data) {
+        console.log('Got note:', data);
+        var note = $('.note:last').clone(true, true);
+        note.find('header').append(data.title);
+        note.find('article').append(data.content);
+
+        if (data.attachments.length > 0) {
+            $.each(data.attachments, function(i) {
+                note.find('footer ul').append(
+                    '<li><a href=\'#\'>' + data.attachments[i].name
+                    + '</a></li>');
+            });
+        } else {
+            note.find('footer').hide();
+        }
+        note.show();
+        $('#notebook').prepend(note)
+        fixLinks();
+    });
+}
+
+function fixLinks() {
+    $('a').click(function(event) {
+        var href_re = /^http:\/\//i;
+        var href = jQuery(this).attr('href');
+        console.log(href);
+        if (href_re.test(href)) {
+            // External link
+            console.log('External link');
+            event.target.target = '_blank';
+        } else {
+            // Internal link
+            console.log('Internal link');
+            event.preventDefault();
+            getItem(href);
+        }
+    });
+}
+
 $(document).ready(function() {
+    // Initialize the accordion widget in the sidebar.
     $('#accordion').accordion({header: 'h3', fillSpace: true});
 
-    getNote = function(name) {
-        $.getJSON('/note/' + name, function(data) {
-            console.log('Got note:', data);
-            var note = $('.note:last').clone(true, true);
-            note.find('header').append(data.title);
-            note.find('article').append(data.content);
-
-            if (data.attachments.length > 0) {
-                $.each(data.attachments, function(i) {
-                    note.find('footer ul').append(
-                        '<li><a href=\'#\'>' + data.attachments[i].name
-                        + '</a></li>');
-                });
-            } else {
-                note.find('footer').hide();
-            }
-            note.show();
-            $('#notebook').prepend(note)
-        });
-    }
-
-    $('a.local').click(function(event) {
-        event.preventDefault();
-        link = jQuery(this);
-        note = link.attr('href').split('/')[2];
-        getNote(note);
-    });
-
+    // Hide the starting template note.
     $('.note').hide();
-    getNote('start');
+
+    getItem('/note/start');
 });
